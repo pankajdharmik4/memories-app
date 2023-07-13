509f446757c1a2b1d027d16121af40c7a7a7a2a5
Pankaj Dharmik <pankajdharmik1999@gmail.com>
2023-07-13 22:08:28 +0530

    Commit

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -16,11 +16,11 @@ function Navbar() {
         </div>
         <Toolbar className={classes.toolbar}>
             {/* {user?( */}
-                    <div className={classes.profile}>
+                    {/* <div className={classes.profile}>
                         <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
                         <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
                         <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
-                    </div>
+                    </div> */}
             {/* // ):
             // (
             //     <Button component={Link} to='/auth' variant="contained" color="primary">Sign In</Button>
